ff4d22d636a91178e7746d09e3ca722149d458d8
Kirsten Bauman <kirstenbauman@Kirstens-MacBook-Pro.local>
2017-04-18 16:33:48 -0700

    s3

--- Assignment-4/server.js
+++ Assignment-4/server.js
@@ -1,17 +1,41 @@
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3();
+
 var express = require("express");
+
 var server = express();
 var bodyParser = require('body-parser');
+//var connect = require("connect");
+
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
-var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/photos');
+//--------------assignment 6 update-----------------
+server.use(methodOverride());
+//app.use(bodyParser());
+server.use(require('connect').bodyParser());
+//server.use(connect.bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+server.use(bodyParser.json())
+
+server.use(express.static(__dirname + '/public'));
+server.use(errorHandler());
+//---------------------------------------------------
 
 server.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
+var db = require('mongoskin').db('mongodb://user:pwd@127.0.0.1:27017/photos');
 
 var todoList = [];
 
@@ -79,6 +103,29 @@ server.get("/getPhoto", function (req, res) {
     res.send(JSON.stringify(result));
   });
 });
+
+server.post('/uploadFile', function(req, res){
+      console.log(req.body);
+        var intname = req.body.fileInput;
+        var filename = req.files.input.name;
+        var fileType =  req.files.input.type;
+        var tmpPath = req.files.input.path;
+        var s3Path = '/' + intname;
+        
+        fs.readFile(tmpPath, function (err, data) {
+            var params = {
+                Bucket:'ame470kb',
+                ACL:'public-read',
+                Key:intname,
+                Body: data,
+                ServerSideEncryption : 'AES256'
+            };
+            s3.putObject(params, function(err, data) {
+                console.log(err);
+                res.end("success");
+            });
+        });
+  });
 
 server.use(methodOverride());
 server.use(bodyParser());
